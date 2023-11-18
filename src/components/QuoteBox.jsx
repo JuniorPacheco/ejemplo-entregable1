216d290b73054559cd0008453ddf8e9007d0afb1
Junior Pacheco <junior.pacheco.356@gmail.com>
2023-11-17 22:49:21 -0500

    project finished

---
+++ src/components/QuoteBox.jsx
@@ -0,0 +1,20 @@
+import "./styles/QuoteBox.css";
+
+const QuoteBox = ({ quote, handleChangeQuote }) => {
+  const { phrase, author } = quote;
+
+  console.log(quote);
+  return (
+    <article className="quotebox">
+      <h1 className="quotebox__title">INFOGALAX</h1>
+      <div className="quotebox__box">
+        <p>{phrase}</p>
+      </div>
+      <button className="quotebox__btn" onClick={handleChangeQuote}>
+        <img src="/boton.svg" alt="" />
+      </button>
+      <span className="quotebox__author">{author}</span>
+    </article>
+  );
+};
+export default QuoteBox;
